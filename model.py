b69d9ded941462924f63881cfab6d72afac9f3a6
mattscicluna <mattcscicluna@gmail.com>
2018-03-25 13:41:27 -0400

    improved the current convert_to_params method

--- model.py
+++ model.py
@@ -17,11 +17,6 @@ class Controller(nn.Module):
     def forward(self, x, r):
         pass
 
-    def size(self):
-        """Returns the size of the controller output (100 for us)
-        """
-        pass
-
 
 class NTMReadHead(nn.Module):
     def __init__(self):
@@ -57,7 +52,7 @@ class NTM(nn.Module):
     Neural Turing Machine
     """
     def __init__(self, num_inputs, num_outputs, controller, attention,
-                 read_head, write_head, memory_size, memory_feature_size):
+                 read_head, write_head, memory_size, memory_feature_size, integer_shift):
         """Initialize the NTM.
         :param num_inputs: External input size.
         :param num_outputs: External output size.
@@ -67,6 +62,7 @@ class NTM(nn.Module):
         :param write_head: list of :class:`NTMWriteHead`
         :param memory_size: N in the paper
         :param memory_feature_size: M in the paper
+        :param integer_shift: allowed integer shift (see pg 8 of paper)
         """
         super(NTM, self).__init__()
 
@@ -78,38 +74,47 @@ class NTM(nn.Module):
         self.write_head = write_head
         self.memory_size = memory_size
         self.memory_feature_size = memory_feature_size
-        self.memory = Variable(np.zeros(shape=(self.memory_size, self.memory_feature_size)))
+        self.integer_shift = integer_shift
+        self.memory = Variable(torch.zeros(self.memory_size, self.memory_feature_size))
 
         # Initialize a fully connected layer to produce the actual output:
-        self.fc = nn.Linear(self.controller.size(), num_outputs)
+        self.fc = nn.Linear(self.num_inputs, self.num_outputs)
 
         # Corresponding to beta, kappa, gamma, g, s, e, a sizes from the paper
-        self.params_lengths = [self.memory_feature_size, 1, 1, 3, 1,
+        self.params = ['beta', 'kappa', 'gamma', 'g', 's', 'e', 'a']
+        self.params_lengths = [1, self.memory_feature_size, 1, 1, self.integer_shift,
                                self.memory_feature_size, self.memory_feature_size]
 
-        self.fc_params = nn.Linear(controller.size(), sum(self.params_lengths))
+        self.fc_params = nn.Linear(self.num_outputs, sum(self.params_lengths))
+
+        # Corresponding to beta, kappa, gamma, g, s, e, a
+        # (choice of activations selected to obey corresponding domain restrictions from the paper)
+        self.activations = {'beta': F.softplus,
+                            'kappa': lambda x: x,
+                            'gamma': lambda x: 1 + F.softplus(x),
+                            'g': F.sigmoid,
+                            's': lambda x: F.softmax(F.softplus(x)),
+                            'e': F.sigmoid,
+                            'a': F.sigmoid}
 
 
     def convert_to_params(self, output):
         """Transform output from controller into parameters for attention and write heads
         :param output: output from controller.
         """
-        params = list()
-        o = self.fc_write(output)
-        #o = Variable(torch.FloatTensor(o))
+        to_return = {'beta': 0,
+                     'kappa': 0,
+                     'gamma': 0,
+                     'g': 0,
+                     's': 0,
+                     'e': 0,
+                     'a': 0}
+        o = self.fc_params(output)
         l = np.cumsum([0] + self.params_lengths)
-        activations = [F.softplus,
-                       lambda x: x,
-                       lambda x: 1 + F.softplus(x),
-                       F.sigmoid,
-                       lambda x: F.softmax(F.softplus(x)),
-                       F.sigmoid,
-                       F.sigmoid]
-        for i in range(len(l)-1):
-            params.append(activations[i](o[l[i]:l[i+1]]))
+        for idx in range(len(l)-1):
+            to_return[self.params[idx]] = self.activations[self.params[idx]](o[l[idx]:l[idx+1]])
 
-        beta, kappa, gamma, g, s, e, a = params
-        return beta, kappa, gamma, g, s, e, a
+        return to_return
 
     def forward(self, x, r):
         """Perform forward pass from the NTM.
@@ -129,6 +134,19 @@ class NTM(nn.Module):
         return output, next_r
 
 
+#  for testing purposes only!
+controller = Controller(network=0)
+attn =NTMAttention()
+read_head = NTMReadHead()
+write_head = NTMWriteHead()
 
+ntm = NTM(num_inputs=10, num_outputs=100, controller=controller,
+          attention=attn, read_head=read_head, write_head=write_head,
+          memory_size=20, memory_feature_size=15, integer_shift=3)
+
+output = Variable(torch.FloatTensor(np.random.rand(100)))
+x = ntm.convert_to_params(output)
+
+print('done')
 
 
