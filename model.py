e6bf52202508c61528525b82db663bb173459376
mattscicluna <mattcscicluna@gmail.com>
2018-03-25 14:37:00 -0400

    added controller, read and write heads and memory constructors all in ntm

--- model.py
+++ model.py
@@ -8,14 +8,15 @@ class Controller(nn.Module):
     """
     controller for NTM
     """
-    def __init__(self, network):
+
+    def __init__(self, network, input_dim, output_dim, num_layers):
         """network: object which takes as input r_t and x_t and returns h_t
         """
         super(Controller, self).__init__()
-        self.network = network
+        self.size = output_dim
 
     def forward(self, x, r):
-        pass
+        return Variable(torch.FloatTensor(np.random.rand(self.size)))
 
 
 class NTMReadHead(nn.Module):
@@ -51,15 +52,11 @@ class NTM(nn.Module):
     """
     Neural Turing Machine
     """
-    def __init__(self, num_inputs, num_outputs, controller, attention,
-                 read_head, write_head, memory_size, memory_feature_size, integer_shift):
+    def __init__(self, num_inputs, num_outputs, controller_size, memory_size, memory_feature_size, integer_shift):
         """Initialize the NTM.
         :param num_inputs: External input size.
         :param num_outputs: External output size.
-        :param controller: :class:`Controller`
-        :param attention: :class:`NTMAttention`
-        :param read_head: list of :class:`NTMReadHead`
-        :param write_head: list of :class:`NTMWriteHead`
+        :param controller_size: size of controller output layer
         :param memory_size: N in the paper
         :param memory_feature_size: M in the paper
         :param integer_shift: allowed integer shift (see pg 8 of paper)
@@ -68,24 +65,30 @@ class NTM(nn.Module):
 
         self.num_inputs = num_inputs
         self.num_outputs = num_outputs
-        self.controller = controller
-        self.attention = attention
-        self.read_head = read_head
-        self.write_head = write_head
+        self.controller_size = controller_size
         self.memory_size = memory_size
         self.memory_feature_size = memory_feature_size
         self.integer_shift = integer_shift
+
+        #  Initialize components
+        self.controller = Controller(network=None, input_dim=self.num_inputs,
+                                     output_dim=self.controller_size, num_layers=1)
+        self.attention = NTMAttention()
+        self.read_head = NTMReadHead()
+        self.write_head = NTMWriteHead()
+
+        #  Initialize memory
         self.memory = Variable(torch.zeros(self.memory_size, self.memory_feature_size))
 
         # Initialize a fully connected layer to produce the actual output:
-        self.fc = nn.Linear(self.num_inputs, self.num_outputs)
+        self.fc = nn.Linear(self.controller_size, self.num_outputs)
 
         # Corresponding to beta, kappa, gamma, g, s, e, a sizes from the paper
         self.params = ['beta', 'kappa', 'gamma', 'g', 's', 'e', 'a']
         self.params_lengths = [1, self.memory_feature_size, 1, 1, self.integer_shift,
                                self.memory_feature_size, self.memory_feature_size]
 
-        self.fc_params = nn.Linear(self.num_outputs, sum(self.params_lengths))
+        self.fc_params = nn.Linear(self.controller_size, sum(self.params_lengths))
 
         # Corresponding to beta, kappa, gamma, g, s, e, a
         # (choice of activations selected to obey corresponding domain restrictions from the paper)
@@ -96,7 +99,6 @@ class NTM(nn.Module):
                             's': lambda x: F.softmax(F.softplus(x)),
                             'e': F.sigmoid,
                             'a': F.sigmoid}
-
 
     def convert_to_params(self, output):
         """Transform output from controller into parameters for attention and write heads
@@ -135,17 +137,11 @@ class NTM(nn.Module):
 
 
 #  for testing purposes only!
-controller = Controller(network=0)
-attn =NTMAttention()
-read_head = NTMReadHead()
-write_head = NTMWriteHead()
-
-ntm = NTM(num_inputs=10, num_outputs=100, controller=controller,
-          attention=attn, read_head=read_head, write_head=write_head,
+ntm = NTM(num_inputs=9, num_outputs=9, controller_size=100,
           memory_size=20, memory_feature_size=15, integer_shift=3)
 
-output = Variable(torch.FloatTensor(np.random.rand(100)))
-x = ntm.convert_to_params(output)
+
+x = ntm.forward(x=0, r=1)
 
 print('done')
 
