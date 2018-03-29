760cff2fdb9e130f8f9810b7d6dde272b3f73c83
mattscicluna <mattcscicluna@gmail.com>
2018-03-29 13:15:48 -0400

    added gradient clipping to ntm

--- train.py
+++ train.py
@@ -95,6 +95,12 @@ def run(learning_rate, batch_size, cuda, memory_feature_size, num_inputs, num_ou
 
         loss = criterion(output, batch)
         loss.backward(retain_graph=True)
+
+        # Clip Gradient between [10, 10].
+        parameters = list(filter(lambda p: p.grad is not None, ntm.parameters()))
+        for p in parameters:
+            p.grad.data.clamp_(-10, 10)
+
         optimizer.step()
 
         print("Current Batch Loss:", round(loss.data[0], 3))
@@ -229,10 +235,10 @@ def run_lstm(learning_rate, batch_size, cuda, num_inputs, num_outputs,
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, default='NTM', help='"NTM" or "LSTM" (baseline)')
-    parser.add_argument('--learn_rate', type=float, default=0.01, help='Learning rate')
+    parser.add_argument('--learn_rate', type=float, default=0.0001, help='Learning rate')
     parser.add_argument('--batch_size', type=int, default=32, help='batch_size')
-    parser.add_argument('--M', type=int, default=20, help='memory feature size')
-    parser.add_argument('--N', type=int, default=128, help='memory size')
+    parser.add_argument('--M', type=int, default=128, help='memory feature size')
+    parser.add_argument('--N', type=int, default=20, help='memory size')
     parser.add_argument('--num_inputs', type=int, default=9, help='number of inputs in NTM')
     parser.add_argument('--num_outputs', type=int, default=9, help='number of outputs in NTM')
     parser.add_argument('--controller_size', type=int, default=100, help='size of controller output of NTM')
